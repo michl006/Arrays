ab0dba191db0cbd191f0d409e24cd9c2dc95236e
michl006 <119848099+michl006@users.noreply.github.com>
2022-12-12 11:17:26 +0100

    Create Arrays.java

---
+++ Arrays.java
@@ -0,0 +1,19 @@
+public class Arrays{
+  public static void main(String[] arg){
+    
+    
+    Datentyp name [];                                             //Deklaration eines einfachen Arrays
+    
+    Datentyp name [][];                                           //Deklaration eines zweidimensionalen Arrays
+    
+    name = new Datentyp [anzahl];                                 //Anlegen eines einfachen Arrays
+   
+    name = new Datentyp [zeilenanzahl][spaltenanzahl];            //Anlegen eines zweidimensionalen Arrays         
+    
+    name[index] = Wert;                                           //Speichern eines Wertes in einem einfachen Arrays
+    
+    name[zeile][spalte] = Wert;                                   //Speichern eines Wertes in einem zweidimensionalen Arrays
+      
+    System.out.println(name[index]);                              //Ausgabe eines einfachen Arrays
+    
+    System.out.println(name[zeile][spalte]);                      //Ausgabe eines zweidimensionalen Arrays
